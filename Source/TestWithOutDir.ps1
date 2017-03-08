c8b260cddc7d61ef42478ccaea841c1e9a6d3cc7
michael-freidgeim-webjet <michael.freidgeim@webjet.com.au>
2017-03-08 18:34:19 +1100

    Source\TestWithOutDir.ps1

--- Source/TestWithOutDir.ps1
+++ Source/TestWithOutDir.ps1
@@ -1,4 +1,8 @@
+#cls
 cd $PSScriptRoot
+	$baseDir = $PSScriptRoot
+	$binariesPath = "$baseDir\Binaries"
+
 .\Build.ps1
 
 # Restore NuGet packages before build
@@ -6,7 +10,8 @@ Push-Location ".\Test\WebApplication1"
 &..\..\Build\NuGet.exe restore
 
 # Build solution with RunOctoPack=true
-&"c:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" WebApplication1.sln /target:Rebuild /p:RunOctoPack=true
+#&"c:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" WebApplication1.sln /target:Rebuild /p:RunOctoPack=true
+&"c:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" WebApplication1.sln  /target:Rebuild /p:RunOctoPack=true /p:OutDir="$binariesPath\"
 
 # Assert nupkg exists
 if (-not (Test-Path "WebApplication1\bin\WebApplication1.1.0.0.nupkg"))
